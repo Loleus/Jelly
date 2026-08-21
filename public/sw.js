4b6531edab7b5e9fda058cda68fa7def79fc4f52
Loleus <loleus@gmail.com>
2026-08-22 00:45:50 +0200

    update0sw

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jelly-v2";
+const CACHE_NAME = "jelly-v3";
 const APP_SHELL = [
   "./",
   "./manifest.webmanifest",
@@ -7,6 +7,7 @@ const APP_SHELL = [
   "./icons/icon-512.png",
   "./icons/icon-1024.png",
   "./fonts/FreckleFace-Regular.woff2",
+  "./fonts/Yarni-Regular.woff2",
 ];
 
 self.addEventListener("install", (event) => {
